cad021796ea5daac6b73f913e6dc2f3b95e3f9c4
adithya-902 <adithyabiju902@gmail.com>
2023-08-31 21:39:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       // home: const Responsivelayout(
       //     webScreenLayout: WebScreenLayout(),
       //     mobileScreenLayout: MobileScreenLayout()),
-      home: const LoginScreen(),
+      home: const SignupScreen(),
     );
   }
 }
